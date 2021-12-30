129fa0bc5c95af8a2342df1ab0acffb13ec95538
krypton99 <32381540+krypton99@users.noreply.github.com>
2021-12-30 21:51:29 +0700

    add worldmap objects definition

---
+++ 05-SceneManager/WorldMapObject.cpp
@@ -0,0 +1,51 @@
+#include "WorldMapObject.h"
+#include "AssetIDs.h"
+#include "Grid.h"
+
+CWorldMapObject::CWorldMapObject(float x, float y, float object_type) :CGameObject(x, y) {
+	this->object_type = object_type;
+	this->type = OBJECT_TYPE_WORLDMAPOBJ;
+
+}
+
+void CWorldMapObject::Render()
+{
+	CAnimations* animations = CAnimations::GetInstance();
+	int aniId = 0;
+	if (GetObjectType() == PLANT_OBJECT)
+		aniId = ANI_PLANT_OBJECT;
+	else if (GetObjectType() == TEXT_OBJECT)
+		aniId = ANI_TEXT_HELP;
+	else
+	{
+		if (vx < 0)
+			aniId = ANI_GUARD_WALK_LEFT;
+		else
+			aniId = ANI_GUARD_WALK_RIGHT;
+	}
+	animations->Get(aniId)->Render(x, y);
+
+	//RenderBoundingBox();
+}
+
+void CWorldMapObject::Update(DWORD dt, vector<LPGAMEOBJECT>* objects)
+{
+	CGameObject::Update(dt);
+	if (GetObjectType() == GUARD_OBJECT)
+	{
+		x += vx*dt;
+		if (x > start_x + BRICK_BBOX || x < start_x)
+		{
+			if (x > start_x + BRICK_BBOX)
+				x = start_x + BRICK_BBOX;
+			if (x < start_x)
+				x = start_x;
+			vx = -vx;
+		}
+	}
+
+}
+void CWorldMapObject::GetBoundingBox(float& l, float& t, float& r, float& b)
+{
+	
+}
---
+++ 05-SceneManager/WorldMapObject.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include "GameObject.h"
+#include "Animation.h"
+#include "Animations.h"
+#include "AssetIDs.h"
+
+#define PLANT_OBJECT	1
+#define TEXT_OBJECT		2
+#define GUARD_OBJECT	3
+
+
+#define ANI_PLANT_OBJECT			19001
+#define ANI_TEXT_HELP				19002
+#define ANI_GUARD_WALK_RIGHT		19003
+#define ANI_GUARD_WALK_LEFT			19004
+
+class CWorldMapObject : public CGameObject {
+protected:
+	float object_type;
+public:
+	CWorldMapObject(float x, float y, float object_type);
+	void SetObjectType(float objectType) { this->object_type = objectType; }
+	float GetObjectType() { return this->object_type; }
+	void Render();
+	void Update(DWORD dt, vector<LPGAMEOBJECT>* objects);
+	void GetBoundingBox(float& l, float& t, float& r, float& b);
+	virtual int IsBlockingX() { return 0; };
+	virtual int IsBlockingY() { return 0; };
+};
