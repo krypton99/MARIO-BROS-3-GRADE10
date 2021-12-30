798b3c0ad5df9a7832c9bb49ab9097cf120dd8dc
krypton99 <32381540+krypton99@users.noreply.github.com>
2021-12-30 21:03:35 +0700

    change HUD position when in worldmap

--- 05-SceneManager/HUD.cpp
+++ 05-SceneManager/HUD.cpp
@@ -9,10 +9,18 @@
 void HUD::Render(float x, float y, CMario* mario) {
 	float vx, vy;
 	mario->GetSpeed(vx, vy);
-	speedBar = new CSpeedBar(x+ 92,y+ SCREEN_HEIGHT/2 + 32);
+	
 	board = CAnimations::GetInstance();
 	background = CAnimations::GetInstance();
-	background->Get(ID_ANI_BACKGROUND)->Render(x + SCREEN_WIDTH/2, y + SCREEN_HEIGHT / 2 + 64);
-	board->Get(ID_ANI_BOARD)->Render(x + BOARD_WIDTH/2 + 8 , y + SCREEN_HEIGHT/2 + 36);
+	if (mario->GetStage() == WORLD_MAP_SCENE) {
+		speedBar = new CSpeedBar(x + 92, y + SCREEN_HEIGHT / 2 + 60);
+		background->Get(ID_ANI_BACKGROUND)->Render(x + SCREEN_WIDTH / 2, y + SCREEN_HEIGHT / 2 + 90);
+		board->Get(ID_ANI_BOARD)->Render(x + BOARD_WIDTH / 2 + 8, y + SCREEN_HEIGHT / 2 + 64);
+	}
+	else {
+		speedBar = new CSpeedBar(x + 92, y + SCREEN_HEIGHT / 2 + 32);
+		background->Get(ID_ANI_BACKGROUND)->Render(x + SCREEN_WIDTH / 2, y + SCREEN_HEIGHT / 2 + 64);
+		board->Get(ID_ANI_BOARD)->Render(x + BOARD_WIDTH / 2 + 8, y + SCREEN_HEIGHT / 2 + 36);
+	}
 	speedBar->Render(vx);
 }
